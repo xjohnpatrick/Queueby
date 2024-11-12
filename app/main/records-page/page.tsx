e62e5ce259f21bc7ded3c7ca40992a6d9855bb8b
Jpi <130180675+JohnPatoriku@users.noreply.github.com>
2024-11-13 02:39:26 +0800

    13.11.24.0239

--- app/main/records-page/page.tsx
+++ app/main/records-page/page.tsx
@@ -9,54 +9,56 @@ import React from 'react'
 const Records = () => {
 
   return (
-    <div className="flex flex-col items-center w-full h-[1100px]">
-      <h1 className="flex text-4xl font-semibold my-8 font-montserrat text-center">
-        Viewing All Records
-      </h1>
+    <div className="flex flex-col items-center justify-center w-full h-[1800px]">
+      <div className="flex flex-col items-center w-[340px] sm:w-[600px] lg:w-[780px] xl:w-[1000px] 2xl:w-[1200px]">
+        <h1 className="flex text-4xl font-semibold my-8 font-montserrat text-center">
+          Viewing All Records
+        </h1>
 
-      <div className="flex flex-col w-[340px] sm:w-[600px] lg:w-[780px] xl:w-[1200px] h-[780px] border border-black">
-        <div className="h-1/4 lg:h-1/3">
-          <div className="flex relative items-center justify-center">
-            <div className="absolute left-2 sm:left-4 lg:left-10 top-10">
-              <div className="lg:w-28 lg:h-28 w-14 h-14 sm:w-20 sm:h-20 relative">
-                <Image src={makatiImg} alt="Makati Logo" fill />
+        <div className="flex flex-col w-full h-[700px] lg:h-[1250px] border border-black">
+          <div className="h-1/4">
+            <div className="flex relative items-center justify-center">
+              <div className="absolute left-2 sm:left-4 lg:left-10 top-10">
+                <div className="lg:w-28 lg:h-28 w-14 h-14 sm:w-20 sm:h-20 relative">
+                  <Image src={makatiImg} alt="Makati Logo" fill />
+                </div>
               </div>
-            </div>
-            <div className="flex flex-col mt-10 text-center font-montserrat">
-              <span className="text-xs sm:text-base lg:text-xl uppercase">
-                Republic of the Philippines
-              </span>
-              <span className="text-xs sm:text-base lg:text-2xl uppercase font-bold">
-                City of Makati
-              </span>
-              <span className="text-xs sm:text-base lg:text-3xl uppercase font-bold">
-                Barangay 28 - Comembo
-              </span>
-              <span className="text-xs sm:text-base lg:text-xl uppercase">
-                Anahaw St., Comembo, Makati City
-              </span>
-              <span className="text-xs sm:text-base lg:text-xl uppercase">
-                Tel. No. 7738-1883 / 7754-3045
-              </span>
-              <span className="text-xs lg:text-4xl font-bold mt-10">
-                All Records
-              </span>
-            </div>
-            <div className="absolute right-2 sm:right-4 lg:right-10 top-10">
-              <div className="relative lg:w-28 lg:h-28 w-14 h-14 sm:w-20 sm:h-20">
-                <Image src={comemboImg} alt="Comembo Logo" fill />
+              <div className="flex flex-col mt-10 text-center font-montserrat">
+                <span className="text-xs sm:text-base lg:text-xl uppercase">
+                  Republic of the Philippines
+                </span>
+                <span className="text-xs sm:text-base lg:text-2xl uppercase font-bold">
+                  City of Makati
+                </span>
+                <span className="text-xs sm:text-base lg:text-3xl uppercase font-bold">
+                  Barangay 28 - Comembo
+                </span>
+                <span className="text-xs sm:text-base lg:text-xl uppercase">
+                  Anahaw St., Comembo, Makati City
+                </span>
+                <span className="text-xs sm:text-base lg:text-xl uppercase">
+                  Tel. No. 7738-1883 / 7754-3045
+                </span>
+                <span className="text-sm sm:text-base lg:text-4xl font-bold mt-4 lg:mt-10">
+                  All Records
+                </span>
+              </div>
+              <div className="absolute right-2 sm:right-4 lg:right-10 top-10">
+                <div className="relative lg:w-28 lg:h-28 w-14 h-14 sm:w-20 sm:h-20">
+                  <Image src={comemboImg} alt="Comembo Logo" fill />
+                </div>
               </div>
             </div>
           </div>
+          <div className="flex-grow h-3/4 items-center justify-center m-4 lg:mx-10 overflow-y-scroll scrollbar-hide bgre">
+            <DashboardTable removeWrapper/>
+          </div>
         </div>
-        <div className="flex-grow h-3/4 lg:h-2/3 items-center justify-center m-4 lg:m-10 overflow-y-scroll">
-          <DashboardTable />
+        <div className="flex mt-4 font-bebas">
+          <Button className="text-4xl bg-blue-400 text-white w-[151px] h-[57px] rounded-xl">
+            Print
+          </Button>
         </div>
-      </div>
-      <div className="flex mt-4 font-bebas">
-        <Button className="text-4xl bg-blue-400 text-white w-[151px] h-[57px] rounded-xl">
-          Print
-        </Button>
       </div>
     </div>
   );
--- components/ui/DashboardTable.tsx
+++ components/ui/DashboardTable.tsx
@@ -11,7 +11,11 @@ import { dashboardColumns } from "@/data/users/columns";
 import { useState } from "react";
 import { dashboardRows } from "@/data/users/rows";
 
-export default function DashboardTable() {
+interface DashboardTableProps {
+  removeWrapper?: boolean;
+}
+
+export default function DashboardTable({ removeWrapper }: DashboardTableProps) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedUser, setSelectedUser] = useState('');
 
@@ -22,36 +26,66 @@ export default function DashboardTable() {
 
   return (
     <>
-    <Table layout="auto" isStriped aria-label="Example table with dynamic content" style={{ height: "700px" }}>
-      <TableHeader columns={dashboardColumns}>
-        {(column) => <TableColumn key={column.key} align="center" className={`text-black text-xl ${bebasNeue.className}`}>{column.label}</TableColumn>}
-      </TableHeader>
-      <TableBody items={dashboardRows} emptyContent={"No contents to display."}>
-        {(item) => (
-           <TableRow key={item.key} className={`text-black text-xl ${montserrat.className}`}>
-            {(columnKey) => (
-              <TableCell className="w-20">
-                {columnKey === 'userId' ? (
-                  <button 
-                    onClick={() => handleUserIdClick(getKeyValue(item, 'userId'))}
-                    className="text-blue-600 hover:text-blue-800 underline focus:outline-none"
-                  >
-                    {getKeyValue(item, columnKey)}
-                  </button>
-                ) : columnKey === 'status' ? (
-                  <span className={`px-2 py-1 rounded ${getStatusClass(item.status)}`}>
-                    {getKeyValue(item, columnKey)}
-                  </span>
-                )  : (
-                  getKeyValue(item, columnKey)
-                )}
-              </TableCell>
-            )}
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
-    <DashboardModal userId={selectedUser} isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+      <Table
+        layout="auto"
+        isStriped
+        aria-label="Example table with dynamic content"
+        style={{ height: "700px" }}
+        {...(removeWrapper && { removeWrapper: true })}
+      >
+        <TableHeader columns={dashboardColumns}>
+          {(column) => (
+            <TableColumn
+              key={column.key}
+              align="center"
+              className={`text-black text-xl ${bebasNeue.className}`}
+            >
+              {column.label}
+            </TableColumn>
+          )}
+        </TableHeader>
+        <TableBody
+          items={dashboardRows}
+          emptyContent={"No contents to display."}
+        >
+          {(item) => (
+            <TableRow
+              key={item.key}
+              className={`text-black text-xl ${montserrat.className}`}
+            >
+              {(columnKey) => (
+                <TableCell className="w-20">
+                  {columnKey === "userId" ? (
+                    <button
+                      onClick={() =>
+                        handleUserIdClick(getKeyValue(item, "userId"))
+                      }
+                      className="text-blue-600 hover:text-blue-800 underline focus:outline-none"
+                    >
+                      {getKeyValue(item, columnKey)}
+                    </button>
+                  ) : columnKey === "status" ? (
+                    <span
+                      className={`px-2 py-1 rounded ${getStatusClass(
+                        item.status
+                      )}`}
+                    >
+                      {getKeyValue(item, columnKey)}
+                    </span>
+                  ) : (
+                    getKeyValue(item, columnKey)
+                  )}
+                </TableCell>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+      <DashboardModal
+        userId={selectedUser}
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
     </>
   );
 }
