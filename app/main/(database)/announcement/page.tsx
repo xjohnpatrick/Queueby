0dd11a84fa516ab689cb2e44c097606b8688fd03
Jpi <130180675+JohnPatoriku@users.noreply.github.com>
2024-11-17 05:33:30 +0800

    17.11.24.0533

--- app/main/(database)/announcement/page.tsx
+++ app/main/(database)/announcement/page.tsx
@@ -6,31 +6,37 @@ export default function Announcement() {
   return (
     <Wrapper className="bg-white">
       <div className="flex w-full h-[500px] justify-center font-bebas">
-        <div className="grid grid-cols-1 place-items-center">
+        <div className="flex flex-col place-items-center gap-4">
           {[
             {
               label: "Slide",
-              placeholder: "Key/UID"
+              placeholder: "Key/UID",
             },
             {
               label: "Title",
-              placeholder: "Slider1"
+              placeholder: "Slider1",
             },
             {
               label: "Url",
-              placeholder: "https://example.com/"
+              placeholder: "https://example.com/",
             },
           ].map((item, index) => (
             <div
               key={index}
-              className="flex flex-col lg:flex-row items-center justify-center gap-0 lg:gap-4"
+              className="grid grid-cols-1 lg:grid-cols-[150px_auto] lg:flex-row items-center justify-center gap-0 lg:gap-4"
             >
-              <label className="text-blue-400 text-2xl lg:text-4xl">{item.label}:</label>
-              <input
-                className="w-[240px] sm:w-[400px] lg:w-[540px] placeholder:text-xl text-xl px-3 py-2 border-2 border-gray shadow-md outline-none rounded-lg font-montserrat"
-                type="password"
-                placeholder={item.placeholder}
-              />
+              <div className="flex">
+                <label className="text-blue-400 text-2xl lg:text-4xl">
+                  {item.label}:
+                </label>
+              </div>
+              <div className="flex">
+                <input
+                  className="w-[240px] sm:w-[400px] lg:w-[540px] placeholder:text-xl text-xl px-3 py-2 border-2 border-gray shadow-md outline-none rounded-lg font-montserrat"
+                  type="password"
+                  placeholder={item.placeholder}
+                />
+              </div>
             </div>
           ))}
           <Button
