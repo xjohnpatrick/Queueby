7659b7b4f89e1d5ed076033dbb74327ae50aaa41
Jpi <130180675+JohnPatoriku@users.noreply.github.com>
2024-11-17 21:25:50 +0800

    17.11.24.2125

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: {
     template: "%s | Cmembo App",
-    default: "Cmembo App",
+    default: "Sign In | Cmembo App",
   },
 };
 
--- app/main/(database)/barangay-officials/page.tsx
+++ app/main/(database)/barangay-officials/page.tsx
@@ -16,8 +16,13 @@ export default function BarangayOfficials() {
               <label className="text-blue-400 text-2xl lg:text-4xl">
                 Punong Barangay
               </label>
-              <div className="flex w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 lg:w-56 lg:h-56 bg-slate-200 relative justify-center">
-                <Image src={punongBarangay.img[0]} alt="Default Img" fill />
+              <div className="flex w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 lg:w-56 lg:h-56 bg-slate-200 relative justify-center rounded-lg">
+                <Image
+                  src={punongBarangay.img[0]}
+                  alt="Default Img"
+                  fill
+                  className="rounded-lg"
+                />
                 <label className="flex text-sm sm:text-base md:text-lg lg:text-xl z-50 text-white absolute bottom-0 lg:bottom-0.5">
                   {punongBarangay.name}
                 </label>
@@ -37,9 +42,14 @@ export default function BarangayOfficials() {
                   kagawad.name.map((individualName, index) => (
                     <div
                       key={`${kagawad.id}-${index}`}
-                      className="flex flex-col w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 bg-slate-200 relative justify-center items-center"
+                      className="flex flex-col w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 bg-slate-200 relative justify-center items-center rounded-lg"
                     >
-                      <Image src={kagawad.img[index]} alt="Default Img" fill />
+                      <Image
+                        src={kagawad.img[index]}
+                        alt="Default Img"
+                        fill
+                        className="rounded-lg"
+                      />
                       <label className="flex text-xs sm:text-sm md:text-base z-50 text-white absolute -bottom-[0.5px] text-nowrap">
                         {individualName}
                       </label>
@@ -59,9 +69,14 @@ export default function BarangayOfficials() {
                 .map((committees) => (
                   <div
                     key={committees.id}
-                    className="flex w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 bg-slate-200 relative justify-center"
+                    className="flex w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 bg-slate-200 relative justify-center rounded-lg"
                   >
-                    <Image src={committees.img[0]} alt="Default Img" fill />
+                    <Image
+                      src={committees.img[0]}
+                      alt="Default Img"
+                      fill
+                      className="rounded-lg"
+                    />
                     <label className="flex text-xs sm:text-sm md:text-base z-50 text-white absolute -bottom-[0.5px]">
                       {committees.name}
                     </label>
